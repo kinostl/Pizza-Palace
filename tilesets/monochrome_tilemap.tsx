bc1f7a41387824350951f5644bb55b0fca442a73
Minty <kinostl@gmail.com>
2023-11-03 02:56:12 -0400

    First commit

---
+++ tilesets/monochrome_tilemap.tsx
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="monochrome_tilemap" tilewidth="16" tileheight="16" spacing="1" tilecount="400" columns="20">
+ <image source="1bit_platformer/Tilemap/monochrome_tilemap.png" width="339" height="339"/>
+ <wangsets>
+  <wangset name="Safety Box" type="mixed" tile="-1">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="110" wangid="1,1,0,0,0,1,1,1"/>
+   <wangtile tileid="111" wangid="1,1,0,0,0,0,0,1"/>
+   <wangtile tileid="112" wangid="1,1,1,1,0,0,0,1"/>
+   <wangtile tileid="130" wangid="0,0,0,0,0,1,1,1"/>
+   <wangtile tileid="132" wangid="0,1,1,1,0,0,0,0"/>
+   <wangtile tileid="150" wangid="0,0,0,1,1,1,1,1"/>
+   <wangtile tileid="151" wangid="0,0,0,1,1,1,0,0"/>
+   <wangtile tileid="152" wangid="0,1,1,1,1,1,0,0"/>
+  </wangset>
+ </wangsets>
+</tileset>
